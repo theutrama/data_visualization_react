f743906adbc23ed78b469e9873cb361616ce0b9f
theutrama <jonaskoch3@gmx.de>
2021-11-25 19:53:27 +0100

    final

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -3,24 +3,36 @@ import React, {Component} from "react";
 import "./Node.css"
 
 export default class Node extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+
 
     render() {
-        const {isFinish, isStart} = this.props;
+        const {
+            col, 
+            row, 
+            isFinish, 
+            isStart, 
+            isWall, 
+            onMouseDown, 
+            onMouseEnter, 
+            onMouseUp
+        } = this.props;
+
         const extraClassName = isFinish
         ? 'node-finish' 
         : isStart
         ? 'node-start' 
+        : isWall
+        ? 'node-wall'
         : '';
     
-        return <div className={`node ${extraClassName}`}></div>;
+        return (
+            <div
+                id={`node-${row}-${col}`} 
+                className={`node ${extraClassName}`}
+                onMouseDown={() => onMouseDown(row, col)}
+                onMouseEnter={() => onMouseEnter(row, col)}
+                onMouseUp={() => onMouseUp()}
+            ></div>
+        );
     }
 }
-
-export const DEFAULT_NODE = {
-    row: 0,
-    col: 0,
-};
